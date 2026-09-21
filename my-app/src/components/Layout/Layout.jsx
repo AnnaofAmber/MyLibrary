ccf74d7f1292b30e796ea37e8ee1a88bcddcafb8
Anna Boichuk <anna.boichuk.o@gmail.com>
2026-09-21 21:43:54 +0300

    adding basic styles to header/footer

--- my-app/src/components/Layout/Layout.jsx
+++ my-app/src/components/Layout/Layout.jsx
@@ -3,15 +3,17 @@ import scss from './Layout.module.scss'
 export const Layout = () => {
 return (
     <div className={scss.wrapper}>
-    <header>
-        <h1>
+        <div className={scss.container}>
+    <header className={scss.header}>
+        <h1 className={scss.headerTitle}>
             MyLibrary
         </h1>
     </header>
     <main></main>
-    <footer>
+    <footer className={scss.footer}>
         <h3>2026</h3>
     </footer>
+    </div>
 </div>
 )
 }
